782129df6fbe0083b2420ba69ace80bb6125ea60
s.odea <s.odea@trustetc.com>
2024-07-30 11:31:27 -0400

    add application tag and env variables to app.py

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-
-import aws_cdk as cdk
-
+from aws_cdk import App, Tags
+from ytown_listings.config import ACCOUNT_ID, REGION
 from ytown_listings.ytown_listings_stack import YtownListingsStack
 
 
-app = cdk.App()
-YtownListingsStack(app, "YtownListingsStack")
+app = App()
+env = {
+    "account": ACCOUNT_ID,
+    "region": REGION,
+}
 
+tags = {
+    "application": "ytown_listings",
+}
+
+ytown_listings_infra_stack = YtownListingsStack(app, "ytown-listings-stack", env=env)
+
+for tag, value in tags.items():
+    Tags.of(app).add(tag, value)
 app.synth()
